adeffc6ea795613a97294b29d4c4be3c739ee6fd
RaissaBarata <raissapereira@sas.com.br>
2021-01-16 15:45:56 -0300

    Add router to login button

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 import ImgCoverDesktop from '../../assets/images/woman-listening.jpg'
 import ImgCoverMobile from '../../assets/images/mobile-login-background.jpg'
@@ -32,27 +33,29 @@ const Login = () => {
       className='login__img'
       src= { isMobile ?
         ImgCoverMobile :
-        ImgCoverDesktop  }
+        ImgCoverDesktop
+      }
       alt= { isMobile ?
-        'Older lady lintening a song with your headphone' :
-        'Black girl smiling listening a song with your headphone' }
+        'Mulher branca, mais velha, com cabelos curtos e vermelhos, usando óculos de sol branco, ouvindo música com headphone também branco' :
+        'Mulher negra, jovem, com cabelos curtos e preto, sorrindo e ouvindo música no seu headphone preto'
+      }
       />
       <div className='login__container'>
         <div className='login__content'>
-        <LogoSpotify aria-label='Green Spotify logo' className='login__logo-spotify'/>
+        <LogoSpotify aria-label='Logo do Spotify verde' className='login__logo-spotify'/>
         <h2 className='login__text'>
           Não toca a música toda<br/> mas toca seu coração
           <span role='img' className='login__heart' aria-label='Coração'>❤️</span>
         </h2>
-        <button aria-roledescription='realiza seu login com spotify' className='login__button' onClick={getAuth}>
-          Entrar com Spotify
-        </button>
+        <Link to='/home'>
+          <button aria-roledescription='realiza seu login com spotify' className='login__button' onClick={getAuth}>
+            Entrar com Spotify
+          </button>
+        </Link>
         </div>
       </div>
     </main>
-
   )
-
 }
 
 export default Login
